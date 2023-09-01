d81f3b972ff8d1e8c4838b993ada6a4bf5221de6
Caitano <josias.m.caitano@accenture.com>
2023-09-01 12:53:49 -0300

    feat: add logic for watch code and move in other project

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
 const chokidar = require('chokidar');
-const { exec } = require('child_process'); // Importe exec do módulo child_process.
+const { exec } = require('child_process');
 const fs = require('fs-extra');
 const path = require('path');
 
 const libPath = "C:\\Users\\josias.m.caitano\\OneDrive - Accenture\\Documents\\projects\\angular\\angular-avancado\\finansys"; // Substitua pelo caminho real para a pasta da sua lib.
 const projectPath = 'C:\\Users\\josias.m.caitano\\OneDrive - Accenture\\Documents\\projects\\angular\\portal-forests\\mfe-base-forests\\node_modules'; // Substitua pelo caminho real para o projeto que utiliza a lib.
-const libName = 'sua-lib'; // Substitua 'sua-lib' pelo nome correto da sua lib.
 
 // Iniciar o processo de build da lib em modo de observação.
 const buildProcess = exec('ng build --watch', { cwd: libPath });
@@ -31,15 +30,30 @@ buildWatcher.on('change', async (changedFile) => {
   console.log(`Detectada alteração em ${changedFile}`);
 
   try {
-    // Copiar a pasta `dist` atualizada diretamente para o projeto principal.
-    const sourceDistPath = path.join(libPath, 'dist');
-    const destinationDistPath = path.join(projectPath, libName, 'dist'); // Removemos 'node_modules' daqui.
-    
-    await fs.remove(destinationDistPath); // Remover a pasta `dist` existente no projeto principal.
-    await fs.copy(sourceDistPath, destinationDistPath); // Copiar a pasta `dist` atualizada.
+    // Obter o nome da pasta da lib dinamicamente.
+    const distContents = await fs.readdir(path.join(libPath, 'dist'));
+    if (distContents.length === 1 && distContents[0] !== 'index.html') {
+      const libName = distContents[0]; // O nome da pasta é o único item na pasta `dist` que não seja o arquivo `index.html`.
 
-    console.log(`Pasta dist copiada para: ${destinationDistPath}`);
+      // Copiar apenas os arquivos existentes da pasta `dist` atualizada diretamente para o projeto principal.
+      const sourceDistPath = path.join(libPath, 'dist', libName);
+      const destinationDistPath = path.join(projectPath, libName);
+
+      // Certificar-se de que a pasta de destino exista antes de copiar.
+      await fs.ensureDir(destinationDistPath);
+
+      const files = await fs.readdir(sourceDistPath);
+      for (const file of files) {
+        const sourceFile = path.join(sourceDistPath, file);
+        const destinationFile = path.join(destinationDistPath, file);
+        await fs.copy(sourceFile, destinationFile);
+      }
+
+      console.log(`Conteúdo da pasta dist copiado para: ${destinationDistPath}`);
+    } else {
+      console.log('Pasta dist não existe ou não pôde ser determinada. Aguardando a criação...');
+    }
   } catch (error) {
-    console.error(`Erro ao copiar a pasta dist: ${error}`);
+    console.error(`Erro ao copiar o conteúdo da pasta dist: ${error}`);
   }
 });
